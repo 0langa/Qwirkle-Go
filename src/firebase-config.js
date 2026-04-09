dcc8d911024b1d59f53f9e97abeff217827a14b5
0langa <olangahd@gmail.com>
2026-04-09 18:02:39 +0200

    Set real Firebase config values
    Updated Firebase configuration with real values.

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,11 +2,12 @@
 // Replace the placeholder values with your real Firebase web app config.
 
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
+  apiKey: "AIzaSyBOt7i1ZewcrY2OMOmkqs6oSKE86-OyYd4",
+  authDomain: "qwirkle-go.firebaseapp.com",
   databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  projectId: "qwirkle-go",
+  storageBucket: "qwirkle-go.firebasestorage.app",
+  messagingSenderId: "684197018933",
+  appId: "1:684197018933:web:c6b38050a2a3d33982a07c",
+
 };
