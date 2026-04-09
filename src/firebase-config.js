00c2a8c5641e6ee8d96c6abe14c1fd80984f8934
0langa <olangahd@gmail.com>
2026-04-09 18:11:55 +0200

    Update Firebase database URL in config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,7 +4,7 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyBOt7i1ZewcrY2OMOmkqs6oSKE86-OyYd4",
   authDomain: "qwirkle-go.firebaseapp.com",
-  databaseURL: "",
+  databaseURL: "https://qwirkle-go-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "qwirkle-go",
   storageBucket: "qwirkle-go.firebasestorage.app",
   messagingSenderId: "684197018933",
